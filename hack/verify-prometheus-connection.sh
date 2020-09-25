3bbb0dea33f537e6b688f8276f1bbb4eef78846b
timflannagan1 <timflannagan@gmail.com>
2020-09-25 17:04:51 -0400

    hack: Always specify the Presto server CLI parameter

--- hack/verify-prometheus-connection.sh
+++ hack/verify-prometheus-connection.sh
@@ -5,7 +5,8 @@ set -eou pipefail
 namespace=${1:-tflannag}
 
 oc -n ${namespace} exec -it $(oc -n ${namespace} get po -l app=presto --no-headers | awk '{ print $1 }') -- \
-    presto-cli \
+    /usr/local/bin/presto-cli \
+    --server http://presto:8080 \
     --catalog prometheus \
     --schema default \
     --execute 'show tables'
